ed89050e998dc8b58eef25dc34d12b70403d4906
Devin Menard <menard.devin@gmail.com>
2026-03-13 10:50:45 -0400

    Branding restoration: Replaced incorrect cyan circle icon with official Prism design (v1.7-BRAND-RESTORED)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo({ className = "w-8 h-8" }: { className?: string }) {
   return (
     <img
-      src="/icon.svg"
+      src="/icon.png"
       alt="Listening Project Logo"
       className={`${className} rounded-lg shadow-lg`}
     />
